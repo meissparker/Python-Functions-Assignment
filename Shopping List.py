5f2869c56050099647721a773306d8fb2e9e5238
meissparker <meissparker@gmail.com>
2024-08-16 21:50:48 -0500

    second commit

--- Shopping List.py
+++ Shopping List.py
@@ -1,17 +1,20 @@
 shopping_list = []
 
+# Task 1 
 def add():
     global shopping_list
     item = input("Enter the item you would like to add to the list: ")
     shopping_list.append(item)
     print(f"{item} has been added to the shopping list.")
 
+# Task 2 
 def remove():
     global shopping_list
     sub = input("Enter the item you would like to remove from the shopping list. ")
     shopping_list.remove(sub)
     print(f"{sub} has been removed from the shopping list.")
 
+# Task 3 
 def view():
     global shopping_list
     print(f" Current Shopping List: {shopping_list}")
